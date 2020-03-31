49eb39a2ca0105e8530d857f3bf84b6a63ea049b
Mario Muñoz <mario.ms90@gmail.com>
2020-03-31 13:40:40 +0200

    Copyright subido

---
+++ ActividadJitpackCopyright/src/main/java/Copyright.java
@@ -0,0 +1,12 @@
+public class Copyright {
+
+    public String getText(){
+        return "Copyright (C) 2020 Mario Muñoz Serrano - All Rights Reserved\n" +
+                "        You may use, distribute and modify this code under the\n" +
+                "        terms of the XYZ license, which unfortunately won't be\n" +
+                "        written for another century.\n" +
+                "        \n" +
+                "        You should have received a copy of the XYZ license with\n" +
+                "        this file.";
+    }
+}
